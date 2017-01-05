b13f20d7e77f246d67cb3be75b6a62b86223ac9a
Gordon Tisher <gordon@balafon.net>
2017-01-04 16:35:09 -0800

    remove old files

--- cleanlibs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Cleaning up Windows libraries for Pony.
-del /q lib\*.lib
-del /q lib\*.exp
-del /q lib\*.pdb
-del /q lib\*.dll
-rmdir /s /q src
--- setenv.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Configuring environment for Pony compilation
-set LIB=%CD%\lib;%LIB%
-set PATH=%PATH%;%CD%\lib
